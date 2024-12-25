af6b7909066d6b5daef4759ea4340f1c04257df3
Rick Petithory <richardpetithory@gmail.com>
2024-12-25 17:58:44 -0500

    CORS

--- shitts/settings.py
+++ shitts/settings.py
@@ -63,7 +63,8 @@ MIDDLEWARE = [
 CORS_URLS_REGEX = r"^/graphql/?$"
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "*",
+    "https://localhost:4000",
+    "https://shitts-app-prs5l.ondigitalocean.app",
 ]
 
 CORS_ALLOW_HEADERS = [
