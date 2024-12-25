fbfd06be3b0ad65048b427a141746cea4b3a160b
Rick Petithory <richardpetithory@gmail.com>
2024-12-25 18:09:16 -0500

    Serve static

--- shitts/urls.py
+++ shitts/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 
 from api import views as api_views
 
@@ -8,3 +10,10 @@ urlpatterns = [
     # path("", shop_views.rent_due),
     path("graphql/", api_views.graphql_view, name="graphql"),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(
+            r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}
+        ),
+    ]
