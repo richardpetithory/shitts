0bbbf5c92c013bf11f984acdd00546ed5e6a23f3
Rick Petithory <richardpetithory@gmail.com>
2024-12-25 20:12:40 -0500

    sdf

--- shitts/settings.py
+++ shitts/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/assets/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"), "frontend/dist")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "frontend/dist"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
