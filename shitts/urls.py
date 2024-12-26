16c1bf3c3c89b4881ec17af8a6e8a9de25ac951c
Rick Petithory <richardpetithory@gmail.com>
2024-12-25 19:56:55 -0500

    Sdf

--- shitts/urls.py
+++ shitts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", serve, {"path": "index.html", "document_root": settings.STATIC_ROOT}),
     path("graphql/", api_views.graphql_view, name="graphql"),
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    re_path(r"^assets/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
